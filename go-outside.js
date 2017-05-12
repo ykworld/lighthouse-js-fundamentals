81be43cb0c146970824779d1485e16411bfbd67b
ykworld <yonseung.kim@gmail.com>
2017-05-12 04:37:13 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,17 @@
+var raining = true;
+var cold = false;
+var temperature = 12;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
